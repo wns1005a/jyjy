fc78da6d4781600ef8747b6e846cc5ef0b48a1f9
wns1005a <72384312+wns1005a@users.noreply.github.com>
2020-10-06 17:19:21 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+// 계산용 객체
+let object = {
+  firstNumber: "",
+  secondNumber: "",
+  operator: null,
+};
+
+// 버튼
+function pushButton(char) {
+  const resultinput = document.getElementById("resultinput");
+  resultinput.value = resultinput.value + char;
+
+  if (char == "+") {
+    object.operator = add;
+  } else if (char == "-") {
+    object.operator = subtract;
+  } else if (char == "*") {
+    object.operator = multiply;
+  } else if (char == "/") {
+    object.operator = divide;
+  } else {
+    if (object.operator == null) {
+      object.firstNumber += char;
+    } else {
+      object.secondNumber += char;
+    }
+  }
+}
+
+// 리셋
+function reset() {
+  const resultinput = document.getElementById("resultinput");
+
+  resultinput.value = "";
+  object = {
+    firstNumber: "",
+    secondNumber: "",
+    operator: null,
+  };
+}
+
+// 계산
+function solve() {
+  const resultinput = document.getElementById("resultinput");
+
+  resultinput.value = object.operator(object.firstNumber, object.secondNumber);
+  object = {
+    firstNumber: "",
+    secondNumber: "",
+    operator: null,
+  };
+}
+
+// 덧셈
+function add(a, b) {
+  return a + b;
+}
+
+// 뺄셈
+function subtract(a, b) {
+  return a - b;
+}
+
+// 곱셈
+function multiply(a, b) {
+  return a * b;
+}
+
+// 나눗셈
+function divide(a, b) {
+  if (b == 0) {
+    return "ERROR";
+  } else {
+    return a / b;
+  }
+}
